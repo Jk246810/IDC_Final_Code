d51f2cb9e366ca8c0a1f82f9b0a437df8ee3d6f1
JK246810 <jameekr22@gmail.com>
2021-04-07 18:00:42 -0400

    added serial read

--- Final_IDC_Code.ino
+++ Final_IDC_Code.ino
@@ -1,9 +1,20 @@
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(Serial.available()){
+    char c = Serial.read(); 
+    if( c == 'w'){
+      //start water sensing tasks 
+    }else if( c == 'e'){
+      //start earth tasks
+    }else if(c == 'f'){
+      //start fire tasks
+    }else if(c == 'a'){
+      //start air tasks 
+    }
+  }
 
 }
